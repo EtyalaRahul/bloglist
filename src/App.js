a8254168de280c8353ee02344153542344466832
EtyalaRahul <2300032656@kluniversity.in>
2025-07-19 04:02:25 +0000

    modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Header from './components/Header'
 import About from './components/About'
